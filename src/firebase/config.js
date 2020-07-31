de9d91edcd051a812569befe03eed68ab568253e
iamfortune <ikechifortune@gmail.com>
2020-07-31 23:20:34 +0100

    added firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBoTJ_i07AzX6DunzcDsG3tTeBqqawpUvg",
+  authDomain: "kay-gram-c2759.firebaseapp.com",
+  databaseURL: "https://kay-gram-c2759.firebaseio.com",
+  projectId: "kay-gram-c2759",
+  storageBucket: "kay-gram-c2759.appspot.com",
+  messagingSenderId: "513387670957",
+  appId: "1:513387670957:web:2c0c4f41eb223c476e0524",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
