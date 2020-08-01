b40913df4ea8ce5696ce95fe50efc7c42bf2096a
iamfortune <ikechifortune@gmail.com>
2020-08-01 23:09:21 +0100

    imported image grid component to the app.js

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -3,9 +3,9 @@ import React from 'react'
 const ImageGrid = () => {
     return (
         <div className="img-grid">
-            
+            images
         </div>
     )
 }
 
-export default ImageGrid
+export default ImageGrid;
