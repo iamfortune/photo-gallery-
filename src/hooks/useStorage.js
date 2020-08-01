526f2d26330102849bc9874a06cee4a365c46c38
iamfortune <ikechifortune@gmail.com>
2020-08-01 22:11:14 +0100

    added the timstamp to the images that are uploaded to the database

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { projectStorage } from '../firebase/config'
+import { projectStorage, projectFirestore, timestamp } from '../firebase/config'
 
 const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
@@ -8,6 +8,7 @@ const useStorage = (file) => {
 
     useEffect(() => {
         const storageRef = projectStorage.ref(file.name);
+        const collectionRef = projectFirestore.collection('images');
 
         storageRef.put(file).on('state_changed', (snap) => {
             let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
@@ -15,7 +16,9 @@ const useStorage = (file) => {
         }, (err) => {
             setError(err)
         }, async () => {
-            const url = await storageRef.getDownloadURL();
+                const url = await storageRef.getDownloadURL();
+                const createdAt = timestamp();
+                collectionRef.add({ url, createdAt })
             setUrl(url)
         })
     }, [file]);
