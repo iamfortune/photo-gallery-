461056fad9c4ad747b7c543a1406f2672a550b45
iamfortune <ikechifortune@gmail.com>
2020-08-01 21:15:35 +0100

    got the file upload working

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import useStorage from '../hooks/useStorage';
 import UseStorage from '../hooks/useStorage'
 
-const ProgressBar = () => {
+const ProgressBar = ({ file, setFile }) => {
+    const { url, progress } = useStorage(file);
+    console.log(progress, url);
+
     return (
         <div className="progress-bar">progress</div>
     )
