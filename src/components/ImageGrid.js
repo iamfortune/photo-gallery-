0255a1d08955800bab0d1ecae4b0e29c95285433
iamfortune <ikechifortune@gmail.com>
2020-08-02 07:50:46 +0100

    added a map feature to the image grid to return array of photos

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import useFirestore from '../hooks/useFireStore'
 
 const ImageGrid = () => {
+    const { docs } = useFirestore('images');
+    console.log(docs)
+
     return (
         <div className="img-grid">
-            images
+            { docs && docs.map(doc => (
+                <div className="img-wrap" key={doc.id}>
+                    <img src={doc.url} alt="uploaded file" />
+                </div>
+            ))}
         </div>
     )
 }
