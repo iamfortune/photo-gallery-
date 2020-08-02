ea1c1235b33febae7cb4eb9fc5ce1795a5d2e5a6
iamfortune <ikechifortune@gmail.com>
2020-08-02 08:38:28 +0100

    improved the index.css, the modal and the app to accept conditional rendering of the modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
 import Title from './components/Title';
 import UploadForm from './components/UploadForm'
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      { selectedImg && <Modal selectedImg={selectedImg} /> }
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = ({ selectedImg }) => {
     return (
         <div className="backdrop">
-            <img src=" " alt="a modal" />
+            <img src={selectedImg} alt="a modal" />
         </div>
     )
 }
